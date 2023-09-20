ee6c84ee9f18702aa424c6b5bc7d936b29ca90c7
VinyEditor <111810869+VinyEditor@users.noreply.github.com>
2023-09-20 19:26:43 -0300

    Update Model.java
    Atualização de validaçãoes nas entidades

--- Spring/demo/src/main/java/com/example/demo/Model.java
+++ Spring/demo/src/main/java/com/example/demo/Model.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Usuario {
@@ -13,6 +15,10 @@ public class Usuario {
     private String username;
     private String password;
     private String email; 
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    private String username;
     
     public Long getId() {
         return id;
@@ -56,6 +62,10 @@ public class Pokemon {
     private String nome;
     private String tipo;
     private int nivel; 
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    private String username;
     
     public Long getId() {
         return id;
@@ -100,6 +110,10 @@ public class CompanheiroPokemon {
     private Pokemon pokemon;
     private int nivelCombate;
     private Date dataCaptura; 
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    private String username;
     
     public Long getId() {
         return id;
@@ -143,6 +157,10 @@ public class EventoCalendario {
     private String titulo;
     private Date data;
     private String local; 
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    private String username;
 
     public Long getId() {
         return id;
